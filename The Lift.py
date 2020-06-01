c6dc52bb4c983eb8e982e108644201a568c14a40
MajewskiL <66209640+MajewskiL@users.noreply.github.com>
2020-06-01 13:13:38 +0200

    Create The Lift.py

---
+++ The Lift.py
@@ -0,0 +1,37 @@
+#https://www.codewars.com/users/Spalding/completed_solutions
+class Dinglemouse(object):
+
+    def __init__(self, queues, capacity):
+
+        self.wys=len(queues)
+        self.ile=capacity
+        self.kolej=[list(x) for x in queues]
+        self.winda=[]
+        pass
+
+    def theLift(self):
+        ptr,kier,stop,output,osob=0,1,1,[0],0
+        for x in self.kolej: osob+=len(x)
+        while osob!=0 or len(self.winda):
+            if len(self.winda)!=0:
+                temp=self.winda.copy()
+                for x in temp:
+                    if x==ptr:
+                        self.winda.remove(x)
+                        osob-=1
+                        stop=1
+            temp=self.kolej[ptr].copy()
+            for x in temp:
+                if ((x>ptr and kier==1) or (x<ptr and kier==-1)):
+                    if len(self.winda)<self.ile:
+                        self.winda.append(x)
+                        self.kolej[ptr].remove(x)
+                    stop=1
+            if stop==1 and output[len(output)-1]!=ptr:
+                output.append(ptr)
+            stop=0
+            ptr+=kier
+            if ptr==self.wys: ptr,kier=ptr-1,-1
+            elif ptr==-1: ptr,kier=0,1
+        if output[len(output)-1]!=0: output.append(0)
+        return (output)
